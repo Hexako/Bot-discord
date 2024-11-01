70103593baf773f5d9b4f77d2e411c62cd0c48f1
Hexako <saintpouicpouic@gmail.com>
2024-11-01 01:31:49 +0100

    ajout fonction blagues

--- MerlinsBot/main.py
+++ MerlinsBot/main.py
@@ -4,6 +4,9 @@ from TOKEN import token
 import random
 import time
 
+from blagues_api import BlaguesAPI, BlagueType
+blagues = BlaguesAPI("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDMxMTgxNjUzNzgwNzI1NzkxIiwibGltaXQiOjEwMCwia2V5IjoiTVRQbEJORFdGNXVYTHRFdjAwUFR1MjNXRHJNY1pra3RqcEEwWUJVRGIxekxmSmpzYUgiLCJjcmVhdGVkX2F0IjoiMjAyMy0xMC0xNlQyMDowMDowNyswMDowMCIsImlhdCI6MTY5NzQ4NjQwN30.99n9pQSqkRjmUvf-aPsRltjW2AbPMYRAgvuCIILoVTk")
+
 
 # intents : en rapport avec les permissions
 intents = discord.Intents.all()
@@ -97,6 +100,17 @@ async def on_message_delete(message):
         title=f'Message supprimé de **{message.author}**',
         description=f'**{message.content}**',
         color=0xFF0000))
+
+# !blague -> Fait des blagues quoi...
+@bot.command(aliases=['b'])
+async def blague(ctx):
+    blague = await blagues.random()
+    await ctx.send(f"*type de blague : {blague.type}*\n{blague.joke}\n||{blague.answer}||")
+
+@bot.command(aliases=['bdev'])
+async def blaguedev(ctx):
+    blague = await blagues.random_categorized(BlagueType.DEV)
+    await ctx.send(f"*type de blague : {blague.type}*\n{blague.joke}\n||{blague.answer}||")
 
 print(f'Lancement du bot...')
 bot.run(token)
